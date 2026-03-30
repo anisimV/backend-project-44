b07a69706dd4228979a1f67fd535c7e50a4db265
Анисии <117117637+anisimV@users.noreply.github.com>
2026-03-30 22:43:42 +0300

    add eslint

---
+++ eslint.config.js
@@ -0,0 +1,20 @@
+import js from '@eslint/js';
+import { defineConfig } from 'eslint/config';
+
+export default defineConfig([
+  {
+    ignores: ['node_modules/**'],
+  },
+  js.configs.recommended,
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: {
+        console: 'readonly',
+        process: 'readonly',
+      },
+    },
+  },
+]);
--- src/cli.js
+++ src/cli.js
@@ -3,7 +3,7 @@ import readLineSync from 'readline-Sync'
 const cli = () => {
     console.log('Welcome to the Brain Games!')
     const name = readLineSync.question('May I have your name? ')
-    console.log('hello ${name}!')
+    console.log(`Hello, ${name}!`);
 }
 
 export default cli
