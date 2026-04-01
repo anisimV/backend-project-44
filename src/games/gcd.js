bb3172ab8c69225c4777d13358348285ec38e952
Анисии <117117637+anisimV@users.noreply.github.com>
2026-04-01 20:51:51 +0300

    add brain-gcd game

---
+++ src/games/gcd.js
@@ -0,0 +1,35 @@
+import runGame from '../index.js';
+
+const gameDescription = 'Find the greatest common divisor of given numbers.'
+
+const getRandomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min
+
+const getGcd = (number1, number2) => {
+    let a = number1
+    let b = number2
+
+    while (b !== 0) {
+        const remainder = a % b;
+        a = b
+        b = remainder
+    }
+
+    return a
+}
+
+const getRoundData = () => {
+    const number1 = getRandomNumber(1, 100)
+    const number2 = getRandomNumber(1, 100)
+
+    const question = `${number1} ${number2}`
+
+    const correctAnswer = String (getGcd(number1, number2))
+
+    return [question, correctAnswer]
+}
+
+const runGcdGame = () => {
+    runGame(gameDescription, getRoundData)
+}
+
+export default runGcdGame
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import readlineSync, { question } from 'readline-sync'
+import readlineSync from 'readline-sync'
 import cli from './cli.js'
 
 const roundsCount = 3
@@ -12,9 +12,9 @@ const runGame = (gameDescription, getRoundData) => {
         const [question, correctAnswer] = getRoundData()
 
         console.log(`Question: ${question}`)
-        const userrAnswer = readlineSync.question('Your answer: ')
+        const userAnswer = readlineSync.question('Your answer: ')
 
-        if (userrAnswer !== correctAnswer) {
+        if (userAnswer !== correctAnswer) {
             console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`)
             console.log(`Let's try again, ${name}!`)
             return
